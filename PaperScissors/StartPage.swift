703b74e9f08f154b199a4ae5e66b014ae7d7d334
Andy Chou <Andy@andys-imac.local>
2016-09-18 01:06:28 +0800

    Link UIButton to VC

--- PaperScissors/StartPage.swift
+++ PaperScissors/StartPage.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class StartPage: UIViewController {
-
+    
+    @IBOutlet var paperB: UIButton!
+    @IBOutlet var scissorB: UIButton!
+    @IBOutlet var stoneB: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //let paperB = UIButton()
+        //paperB.setTitle("paperButton", forState: .Normal)
+        //self.view.addSubview(paperB)
+        //paperB.addTarget(self, action: "WinLose", forControlEvents: UIControlEvents.TouchUpInside)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func WinLose(){
+    
+    
+    
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
