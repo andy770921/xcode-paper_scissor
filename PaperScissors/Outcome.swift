2bf565c934814256ab6ed112ead61f611d390cee
Andy Chou <Andy@andys-imac.local>
2016-09-18 03:13:42 +0800

    Final Version

--- PaperScissors/Outcome.swift
+++ PaperScissors/Outcome.swift
@@ -34,7 +34,38 @@ class Outcome: UIViewController {
                     self.ImageDisp.image = nil
                     self.WLText.text = "Unknown"
             }
-        } else {
+        }
+    
+        else if let WLValueForStone2 = self.WLValueForStone {
+            switch WLValueForStone2 {
+            case 1:
+                self.ImageDisp.image = UIImage(named: "StSc")
+                self.WLText.text = "Stone crushes scissor. You Win"
+            case 2:
+                self.ImageDisp.image = UIImage(named: "StPa")
+                self.WLText.text = "Paper covers stone. You Lose"
+            default:
+                self.ImageDisp.image = nil
+                self.WLText.text = "Unknown"
+        
+        }
+        }
+        else if let WLValueForSc2 = self.WLValueForSc {
+            switch WLValueForSc2 {
+            case 1:
+                self.ImageDisp.image = UIImage(named: "ScPa")
+                self.WLText.text = "Scissor cuts paper. You Win"
+            case 2:
+                self.ImageDisp.image = UIImage(named: "StSc")
+                self.WLText.text = "Stone crushes scissor. You Lose"
+            default:
+                self.ImageDisp.image = nil
+                self.WLText.text = "Unknown"
+        }
+        }
+        
+        
+        else {
             self.ImageDisp.image = nil
         }
         
--- PaperScissors/StartPage.swift
+++ PaperScissors/StartPage.swift
@@ -48,8 +48,14 @@ class StartPage: UIViewController {
         let controller = segue.destinationViewController as! Outcome
         controller.WLValueForStone = self.rollValue()
         }
+        else if segue.identifier == "SegueForScissor"{
+            let controller = segue.destinationViewController as! Outcome
+            controller.WLValueForSc = self.rollValue()
+        }
     }
+   
     
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
