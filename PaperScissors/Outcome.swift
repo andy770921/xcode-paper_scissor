dff47fafd8376fb3c0bb115a562098f84d3297ef
Andy Chou <Andy@andys-imac.local>
2016-09-17 23:49:08 +0800

    Add 2nd VC

---
+++ PaperScissors/Outcome.swift
@@ -0,0 +1,24 @@
+//
+//  Outcome.swift
+//  PaperScissors
+//
+//  Created by Andy Chou on 2016/9/17.
+//  Copyright © 2016年 Andy Chou. All rights reserved.
+//
+
+import UIKit
+
+class Outcome: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
